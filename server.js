1744063229380c2e85c71a631a30f21902b2db4f
Oleh Hatsenko <40500897+IRONKAGE@users.noreply.github.com>
2020-02-04 18:59:43 +0200

    add socket.io
    add socket.io

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 var express = require('express');
-
 const bodyParser = require('body-parser');
 let jwt = require('jsonwebtoken');
 let config = require('./src/config/config');
 let middleware = require('./src/middleware/middleware');
 
 class HandlerGenerator {
-    login (req, res) {
-        let username = req.body.username;
-        let password = req.body.password;
+    login (request, response) {
+        let username = request.body.username;
+        let password = request.body.password;
         let mockedUsername = 'admin';
         let mockedPassword = 'password';
         if (username && password) {
@@ -17,29 +16,33 @@ class HandlerGenerator {
                     config.secret,
                     {expiresIn: '24h'}
                 );
-                res.json({
+                response.json({
                     success: true,
                     message: 'Авторизація успішна!',
                     token: token
                 });
             } else {
-                res.send(403).json({
+                response.send(403).json({
                     success: false,
                     message: 'Не коректний пароль чи ім\'я користувача'
                 });
             }
         } else {
-            res.send(400).json({
+            response.send(400).json({
                 success: false,
                 message: 'Хибна авторизація =('
             });
         }
-      }
-      index (req, res) {
-          res.json({
-              success: true,
-              message: 'Головна сторінка'
-          });
+    }
+    index (request, response) {
+
+        const io = require("socket.io-client");
+        var clientIO = io.connect("http://localhost:15000");
+
+        response.json({
+            success: true,
+            message: 'Головна сторінка'
+        })
     }
 }
 
@@ -47,6 +50,17 @@ function main () {
     let app = express();
     var port = process.env.PORT || 15000;
     var server = require('http').createServer(app);
+
+    var io = require("socket.io")(server);
+
+    io.on("connection", (socket) => {
+        console.info(`Клієнт приєднався [id=${socket.id}]`);
+    
+        socket.on("disconnect", () => {
+            console.info(`Клієнт від'єднався [id=${socket.id}]`);
+        });
+    });
+
     let handlers = new HandlerGenerator();
     app.use(bodyParser.urlencoded({
         extended: true
